9f139747517212a497c43388e3eec2806c1e855e
rpinor <rpinor@unjbg.edu.pe>
2024-07-08 11:09:47 -0500

    Añadiendo codigo faltante del registros1.cpp

--- registros1.cpp
+++ registros1.cpp
@@ -4,16 +4,38 @@ int main(){
     int n;
     cin>>n;
 	struct empleado{
-    char nombre[60];
+    string nombre[60];
     char sexo[60];
     int sueldo[60];
     };
+    int SueldoMin=0;
+    int SueldoMax=1000000;
     empleado emp;
     for(int i=0; i<n; i++){
-        cout<<"Nombre del empleado:";
+        cout<<"Nombre del empleado:"<<endl;
         cin>>emp.nombre[i];
-        cout<<"Sexo del empleado:";
+        cout<<"Sexo del empleado:"<<endl;
         cin>>emp.sexo[i];
-        cout<<"Sueldo del empleado:";
+        cout<<"Sueldo del empleado:"<<endl;
         cin>>emp.sueldo[i];
+        if(emp.sueldo[i]>SueldoMin){
+            SueldoMin=emp.sueldo[i];
+        }
+        if(emp.sueldo[i]<SueldoMax){
+            SueldoMax=emp.sueldo[i];
+        }
     }
+    for(int i=0; i<n; i++){
+        if(emp.sueldo[i]==SueldoMin){
+            cout<<emp.nombre[i]<<endl;
+            cout<<emp.sexo[i]<<endl;
+            cout<<emp.sueldo[i]<<endl;
+        }
+        if(emp.sueldo[i]==SueldoMax){
+            cout<<emp.nombre[i]<<endl;
+            cout<<emp.sexo[i]<<endl;
+            cout<<emp.sueldo[i]<<endl;
+        }
+    }
+    return 0;
+}
